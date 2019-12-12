3175c3e9d05c924f44051a244b3ca479fccbf9ee
Leethring <15216601917@163.com>
2019-12-12 12:08:31 +0000

    p5 completed

--- chapter6/p5.c
+++ chapter6/p5.c
@@ -8,19 +8,21 @@
 #include<stdio.h>
 int main(void)
 {
-    char i, j, letter;
-    int time;
+    char letter;
+    int i, j, time;
 
     printf("Enter a uppercase letter:");
     scanf("%c", &letter);
     time = (int) letter - (int) 'A';
+    printf("%d", time);
+    printf("%c\n", 'A' + 1);
     for (i = 0; i <= time ; i++ )
     {
-        for (j = 0; j - i <= time; j++)
+        for (j = 0; j + i <= time; j++)
             printf(" ");
-        for (j = 0; j <= i ; j++);
+        for (j = 0; j <= i ; j++)
             printf("%c", 'A' + j);
-        for (j = 0; j < i; j++);
+        for (j = 0; j < i; j++)
             printf("%c", 'A' + i -j - 1);
         printf("\n");
 
